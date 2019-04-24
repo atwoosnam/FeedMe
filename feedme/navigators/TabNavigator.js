b642c8374aecb9d97304555352979c38441644aa
ATW <andrewt.woosnam@gmail.com>
2019-04-24 11:12:42 -0400

    Added tab icons

--- feedme/navigators/TabNavigator.js
+++ feedme/navigators/TabNavigator.js
@@ -1,19 +1,27 @@
+import React from 'react';
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 import ShoppingListScreen from '../screens/ShoppingListScreen';
 import StackNavigator from './StackNavigator';
-
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Tabs = createBottomTabNavigator({
     HomeScreen: {
         screen: StackNavigator,
         navigationOptions: {
-            title: "Home",
+            title: "Recipes",
+            tabBarIcon: ({ tintColor }) => (
+                <MaterialCommunityIcons name="rice" size={26} style="outline" color="dimgrey" />
+            )
         }
     },
+    
     ShoppingListScreen: {
         screen: ShoppingListScreen,
         navigationOptions: {
             tabBarLabel: "Shopping List",
+            tabBarIcon: ({ tintColor }) => (
+                <MaterialCommunityIcons name="cart-outline" size={26} color="dimgrey" />
+            )
         }
     }
 });
